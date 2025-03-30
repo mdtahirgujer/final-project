7e3304bff3b9f31e8646ee4ec50fb52d488a24af
mdtahirgujer <mdtahirgujer@gmail.com>
2025-03-30 13:51:59 +0000

    Add 22.py

---
+++ src/22.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def square_matrix(matrix):
+    """
+    Function to calculate the square of a matrix.
+    
+    Args:
+    - matrix: A 2D numpy array representing the input matrix.
+    
+    Returns:
+    - result: The square of the input matrix.
+    """
+    return np.square(matrix)
+
