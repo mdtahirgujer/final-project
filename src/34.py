71ca4772601e0ffd9958eb16c310352967d87d94
mdtahirgujer <mdtahirgujer@gmail.com>
2025-04-25 22:36:14 +0000

    Add 34.py

---
+++ src/34.py
@@ -0,0 +1,10 @@
+class MyClass:
+    def __init__(self):
+        self.some_variable = "initial value"
+
+    def some_method(self):
+        print("This is a method")
+
+my_instance = MyClass()
+print(my_instance.some_variable)
+my_instance.some_method()
